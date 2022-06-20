85390ac77234086dfb2bea83c4436c7188b586dd
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-06-20 10:06:58 +0200

    Update Program.cs

--- BattleSimulator/BattleSimulator/Program.cs
+++ BattleSimulator/BattleSimulator/Program.cs
@@ -8,7 +8,13 @@ namespace BattleSimulator
         static int DiceRoll(int numberOfRolls, int diceSides, int fixedBonus = 0)
         {
             var random = new Random();
-            return (numberOfRolls * random.Next(1, diceSides +1)) + fixedBonus;
+            int sum = 0;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                sum += random.Next(1, diceSides + 1);
+            }
+             return sum += fixedBonus;
+
         }
 
         static void SimulateBattle(List<string> heroNames, string monsterName, int monsterHP, int savingThrowDC)
