a91bd434877ab8a308aa6c1a9a7642b69c2dfcaa
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-06-20 15:26:45 +0200

    dictionaries 1

--- dictionariesPractice/Dictionaries/Program.cs
+++ dictionariesPractice/Dictionaries/Program.cs
@@ -5,11 +5,52 @@ namespace Dictionaries
 {
     internal class Program
     {
-        static void generateQuestion()
+        static Random random = new Random();
+        static void Ask(Dictionary<int, string> games, string gamesType)
         {
-            
-            //Set up dictionaries
-            var random = new Random();
+            var years = new List<int>(games.Keys);
+            int yearIndex = random.Next(games.Count);
+            int year = years[yearIndex];
+            Console.WriteLine($"Which country hosted the olympic {gamesType} games in {year}?");
+            string answer = Console.ReadLine();
+            string correctAnswer = games[year];
+
+            if (answer.ToLowerInvariant() == correctAnswer.ToLowerInvariant())
+            {
+                Console.WriteLine("Correct!");
+            }
+
+            else
+            {
+                Console.WriteLine($"Incorrect! The olympic {gamesType} games in {year} where hosted in {correctAnswer}");
+            }
+            games.Remove(year);
+        }
+
+        static void GenerateQuestion(Dictionary<int, string> summerGames, Dictionary<int, string> winterGames)
+        {
+            //Randomize question (summer or winter?)
+            int type = random.Next(2);
+
+            if (type == 1)
+            {
+                
+
+                Ask(summerGames, "summer");
+            }
+            else
+            {
+                
+
+                Ask(winterGames, "winter");
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
             var winterGames = new Dictionary<int, string>()
             {
                 {2002, "United States"},
@@ -29,62 +70,9 @@ namespace Dictionaries
                 {2016, "Brazil" },
                 {2020, "Japan" }
             };
-
-            //Randomize question (summer or winter?)
-            for (int i = 0; i < 1; i++)
-            { 
-            int type = random.Next(1, 3);
-
-            // Randomize which year
-            int summerYear = 2000 + random.Next(summerGames.Count) * 4;
-            int winterYear = 2002 + random.Next(winterGames.Count) * 4;
-
-              
-
-                if (type == 1)
-                {
-                    Console.WriteLine($"Which country hosted the olympic summer games in {summerYear}?");
-                    string answer = Console.ReadLine();
-                    string correctAnswer = summerGames[summerYear];
-
-                    if (answer.ToLowerInvariant() == correctAnswer.ToLowerInvariant())
-                    {
-                        Console.WriteLine("Correct!");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine($"Incorrect! The olympic summer games in {summerYear} where hosted in {correctAnswer}");
-                    }
-                    summerGames.Remove(summerYear);
-            }
-                if (type == 2)
-                {
-                    Console.WriteLine($"Which country hosted the olympic winter games in {winterYear}?");
-                    string answer = Console.ReadLine();
-                    string correctAnswer = winterGames[winterYear];
-
-                    if (answer.ToLowerInvariant() == correctAnswer.ToLowerInvariant())
-                    {
-                        Console.WriteLine("Correct!");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine($"Incorrect! The olympic winter games in {winterYear} where hosted in {correctAnswer}");
-                    }
-                    winterGames.Remove(winterYear);
-                }
-            }
-
-        }
-
-
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
-                generateQuestion();
+                GenerateQuestion(summerGames, winterGames);
             }
         }
     }
--- dictionariesPractice/Dictionaries2/Program.cs
+++ dictionariesPractice/Dictionaries2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Dictionaries2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var capitals = new Dictionary<string, string>()
+            {
+                {"China", "Bejing" },
+                {"Japan", "Tokyo" },
+                {"Democratic Republic of Kongo", "Kinshasa" },
+                {"Indonesia", "Jakarta"},
+                {"Egypt", "Cairo"},
+                {"South Korea", "Seoul"},
+                {"Mexico", "Mexico City"},
+                {"United Kingdom", "London"},
+                {"Bangladesh", "Dhaka"},
+                {"Peru", "Lima"},
+                {"Iran", "Theran"},
+                {"Thailand", "Bangkok"},
+                {"Vietnam", "Hanoi"},
+                {"Iraq", "Baghdad"},
+                {"Saudi Arabia", "Riyadh"},
+                {"Colombia", "Bogota"},
+                {"Chile", "Santiago"},
+                {"Turkey", "Ankara"},
+            };
+        
         }
     }
 }
