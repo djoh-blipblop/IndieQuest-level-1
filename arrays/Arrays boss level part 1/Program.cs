0dec299da2c2a7e8036c8902dc56224c4f35a840
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-06-21 09:51:38 +0200

    Work on arrays

---
+++ arrays/Arrays boss level part 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arrays_boss_level_part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ arrays/arrays 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays_1
+{
+    internal class Program
+    {
+        static void CreateDayDescription(int day, int season, int year)
+        {
+            string[] days = {"1", "2", "3", "4"};
+            string[] seasons = {"Spring", "Summer", "Fall", "Winter"};
+            string[] years = {"130", "312", "241", "156"};
+            Console.WriteLine($"It is the {days[day]} day of {seasons[season]} of the year {years[year]}");
+           
+        }
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (int i = 0; i < 5; i++)
+            CreateDayDescription(random.Next(0, 4), random.Next(0, 4), random.Next(0, 4));
+        }
+    }
+}
