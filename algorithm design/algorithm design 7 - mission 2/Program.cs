88ab47cc717a1d89b22666ef211f5db478499d60
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-07-12 15:03:05 +0200

    algorithm design

---
+++ algorithm design/algorithm design 7 - mission 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithm_design_7___mission_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
