7d6df161d333fc51ebb84968a0c1efe5d2a4440d
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-07-21 14:55:57 +0200

    more stuff

---
+++ Regex/Regex 3  - 1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Regex_3____1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] monsterManualArray = File.ReadAllLines("MonsterManual.txt");
+            string monsterManual = File.ReadAllText("MonsterManual.txt");
+
+            var namesByAlignment = new List<string>[3, 3];
+            var namesOfUnaligned = new List<string>();
+            var namesOfAnyAlignment = new List<string>();
+            var namesOfSpecialCases = new List<string>();
+
+            for (int axis1 = 0; axis1 < 3; axis1++)
+
+                for (int axis2 = 0; axis2 < 3; axis2++)
+
+                    namesByAlignment[axis1, axis2] = new List<string>();
+
+
+
+            MatchCollection match = Regex.Matches(monsterManual, @"((chaotic)|(neutral)|(lawful)) ((evil)|neutral)|(good)");
+
+            var axis1Values = new[] { "chaotic", "neutral", "lawful" };
+            var axis2Values = new[] { "evil", "neutral", "good" };
+
+            string axis1Text = match.Groups[1].Value;
+            string axis2Text = match.Groups[2].Value;
+            int axis1 = Array.IndexOf(axis1Values, axis1Text);
+            int axis2 = Array.IndexOf(axis2Values, axis2Text);
+            namesByAlignment[axis1, axis2].Add(monsterName);
+
+
+
+
+            for (int i = 0; i < monsterManualArray.Length; i++)
+            {
+                if (Regex.IsMatch(monsterManualArray[i], @"(unaligned)"))
+                {
+                    namesOfUnaligned.Add(monsterManualArray[i - 1]);
+                }
+                else if (Regex.IsMatch(monsterManualArray[i], @"(any alignment)"))
+                {
+                    namesOfAnyAlignment.Add(monsterManualArray[i - 1]);
+                }
+            }
+
+            Console.WriteLine("The names of monsters that are unaligned are:");
+            Console.WriteLine("---------------------------------------------");
+            foreach (string name in namesOfUnaligned)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("The names of monsters that can be of any alignment are:");
+            Console.WriteLine("-------------------------------------------------------");
+            foreach (string name in namesOfAnyAlignment)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
+
+
---
+++ Regex/regex 2 - mission 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace regex_2___mission_3
+{
+    internal class Program
+    {
+
+        static List<string> monsterNames = new List<string>();
+
+        static void Main(string[] args)
+        {
+            //Setting up the list of strings that is the monster manual
+            string[] monsterManual = File.ReadAllLines("MonsterManual.txt");
+
+            //Initalizing the list that will store the alignments
+            List<string> alignment = new List<string>();
+
+            //Setting which character we will split the string at later
+            char[] delimiterChars = { ',' };
+
+            //Let's go trough the list line by line
+            for (int i = 0; i < monsterManual.Length; i++)
+            {
+                //If we find a line that contains a specified alignment, do the following
+                if (Regex.IsMatch(monsterManual[i], "(chaotic|neutral|lawful) (good|neutral|evil)"))
+                {
+                    //Store the whole line in a string
+                    string text = monsterManual[i];
+
+                    // Split the string using the comma(s)
+                    string[] words = text.Split(delimiterChars);
+
+                    //Go up one line to find the name of the monster, add it to the list of names
+                    monsterNames.Add(monsterManual[i - 1]);
+
+                    //Add the alignment. We want to trim the whitespace after the comma, and in some cases (shapeshifters) there are multiple commas in the line
+                    //So we need to make sure to just grab the last part of the string and add it to the list of alignments
+                    alignment.Add(words[words.Length - 1].Trim());
+
+                }
+            }
+            //Print both lists (monster names and alignments)
+            Console.WriteLine("Aligned monsters in the manual are:");
+
+            for (int i = 0; i < monsterNames.Count; i++)
+            {
+                Console.WriteLine($"{monsterNames[i]} ({alignment[i]})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
