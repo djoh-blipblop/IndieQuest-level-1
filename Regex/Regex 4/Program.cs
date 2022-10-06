75c9167a04ae6e5fc7866c29f54cf8dc55411b52
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-10-06 16:46:38 +0200

    regex

---
+++ Regex/Regex 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Regex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            string htmlCode = httpClient.GetStringAsync(@"https://store.steampowered.com/app/553420/TUNIC/").Result;
+            Match review = Regex.Match(htmlCode, @"((?:game_review_summary).\w+"">)(\w* ?\w*)");
+            string score = review.Groups[2].Value;
+            Console.WriteLine($"The current rating of the game Tunic is {score}");
+        }
+    }
+}
