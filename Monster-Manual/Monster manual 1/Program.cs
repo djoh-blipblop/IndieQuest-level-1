49b70b5dc0a563425f667f0812d7532762d5b2f4
djoh-blipblop <106751663+djoh-blipblop@users.noreply.github.com>
2022-10-07 16:22:55 +0200

    regex completed, monster manual begun

---
+++ Monster-Manual/Monster manual 1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Monster_manual_1
+{
+    internal class Program
+    {
+        class MonsterEntry
+        {
+            public string Name;
+            public string Description;
+            public string Alignment;
+            public string HPDefault;
+            public string HPRoll;
+            public string ArmorClass;
+            public string ArmorType;
+            public string Speed;
+            public string BurrowingSpeed;
+            public string FlyingSpeed;
+            public string SwimmingSpeed;
+            public string ChallengeRating;
+            public string XPValue;
+
+        }
+        static void Main(string[] args)
+        {
+            /*Setting up the lists etc.*/
+            string[] monsterManualLines = File.ReadAllLines("MonsterManual.txt");
+            string monsterManual = File.ReadAllText("MonsterManual.txt");
+
+            var monsters = new List<MonsterEntry>();
+
+            /*Setting up the Regex pattern */
+            MatchCollection matches = Regex.Matches(monsterManual,
+                @"\n\n(.*)\n" + /* Monster name*/
+                @"(.*), (.*)\n" + /*Description and Alignment*/
+                @"Hit Points: (\d*) \((.*)\) ?\n" + /*Hitpoints and then Rolled HitPoints*/
+                @"Armor Class: (\d*)(?: \((.*)\))? ?\n" + /*Armor class and then eventual armor type*/
+                @""
+
+            );
+
+            /* Adding the data from the captures into the MonsterEntry class */
+            foreach (Match match in matches)
+            {
+                monsters.Add(new MonsterEntry
+                {
+                    Name = match.Groups[1].Value,
+                    Description = match.Groups[2].Value,
+                    Alignment = match.Groups[3].Value,
+                    HPDefault = match.Groups[4].Value,
+                    HPRoll = match.Groups[5].Value,
+                    ArmorClass = match.Groups[6].Value,
+                    ArmorType = match.Groups[7].Value,
+                    Speed = match.Groups[8].Value,
+                    BurrowingSpeed = match.Groups[9].Value,
+                    FlyingSpeed = match.Groups[10].Value,
+                    SwimmingSpeed = match.Groups[11].Value,
+                    ChallengeRating = match.Groups[12].Value,
+                    XPValue = match.Groups[13].Value,
+                }); ;
+            }
+
+
+            /* Test to see that everything gets recorded properly, remove later */
+            foreach (MonsterEntry monster in monsters)
+            {
+                Console.WriteLine(monster.Name);
+                Console.WriteLine(monster.Description);
+                Console.WriteLine(monster.Alignment);
+                Console.WriteLine(monster.HPDefault);
+                Console.WriteLine(monster.HPRoll);
+                Console.WriteLine(monster.ArmorClass);
+                Console.WriteLine(monster.ArmorType);
+                Console.WriteLine(monster.Speed);
+                Console.WriteLine(monster.BurrowingSpeed);
+                Console.WriteLine(monster.FlyingSpeed);
+                Console.WriteLine(monster.SwimmingSpeed);
+                Console.WriteLine(monster.ChallengeRating);
+                Console.WriteLine(monster.XPValue);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Regex/Regex 4/Program.cs
+++ Regex/Regex 4/Program.cs
@@ -9,10 +9,18 @@ namespace Regex_4
         static void Main(string[] args)
         {
             var httpClient = new HttpClient();
-            string htmlCode = httpClient.GetStringAsync(@"https://store.steampowered.com/app/553420/TUNIC/").Result;
-            Match review = Regex.Match(htmlCode, @"((?:game_review_summary).\w+"">)(\w* ?\w*)");
-            string score = review.Groups[2].Value;
-            Console.WriteLine($"The current rating of the game Tunic is {score}");
+            string[] games = { "https://store.steampowered.com/app/553420/TUNIC/", "https://store.steampowered.com/app/1657630/Slime_Rancher_2/", "https://store.steampowered.com/app/1811260/EA_SPORTS_FIFA_23/", "https://store.steampowered.com/app/1954200/Kena_Bridge_of_Spirits/" };
+
+            for (int i = 0; i < games.Length; i++)
+            {
+                string htmlCode = httpClient.GetStringAsync(games[i]).Result;
+                Match name = Regex.Match(htmlCode, @"<title>(.*) on Steam<");
+                Match review = Regex.Match(htmlCode, @"game_review_summary \w*"">(.*)<");
+                string score = review.Groups[1].Value;
+                string title = name.Groups[1].Value.Trim();
+                Console.WriteLine($"The current rating of the game {title} is {score}");
+            }
+
         }
     }
 }
